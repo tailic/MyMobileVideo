8d9c5c380b0466def1909d35906d90bc29cabef4
MrIYoes <steffen.brauer@gmx.net>
2010-11-08 13:04:52 +0100

    stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,8 +29,7 @@ class User < ActiveRecord::Base
   has_many :articles
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,:registerable,:recoverable, :rememberable, :trackable, :validatable, :http_authenticatable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :name, :password, :password_confirmation, :remember_me, :role, :asset
