f8e58021c45278a4d4fb3c0d56366a774624f22e
MrIYoes <steffen.brauer@gmx.net>
2010-11-22 10:54:36 +0100

    dario hat rumgeheult

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base  
   helper :all
   
-  protect_from_forgery
+  #protect_from_forgery
   
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = exception.message
