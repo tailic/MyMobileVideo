9ced6a2965f74649314c407277fa3225061b8eeb
niklas <niklas@niklas-laptop.(none)>
2010-12-30 17:42:22 +0100

    database.yml for mysql ffmepeg audio codecs

--- app/models/article.rb
+++ app/models/article.rb
@@ -70,20 +70,20 @@ class Article < ActiveRecord::Base
     if self.asset_file_name.include? ".flv"
       mp4 = File.join(File.dirname(asset.path), "#{self.asset_file_name.gsub(".flv",'').gsub(".FLV",'')}.mp4")
       File.open(mp4, 'w')
-      system "ffmpeg -i #{ asset.path } -ar 44100 -ab 128000 -s 640x480 -vcodec mpeg4 -aspect 16:9 -r 25 -qscale 8 -f mp4 -y #{ mp4 }"
+      system "ffmpeg -i #{ asset.path } -ar 44100 -ab 128000 -s 640x480 -vcodec mpeg4 -acodec libfaac -aspect 16:9 -r 25 -qscale 8 -f mp4 -y #{ mp4 }"
       grab_screenshot_from_video
     elsif self.asset_file_name.include? ".mp4"
       flv = File.join(File.dirname(asset.path), "#{self.asset_file_name.gsub(".mp4",'').gsub(".MP4",'')}.flv")
       File.open(flv, 'w')
-      system "ffmpeg -i #{ asset.path }  -ar 22050 -ab 192000 -s 480x360 -vcodec flv -r 25 -qscale 8 -f flv -y #{ flv }"
+      system "ffmpeg -i #{ asset.path }  -ar 22050 -ab 192000 -s 480x360 -vcodec flv -acodec libmp3lame -r 25 -qscale 8 -f flv -y #{ flv }"
       grab_screenshot_from_video
     else
       flv = File.join(File.dirname(asset.path), "#{self.asset_file_name.gsub(".mov",'').gsub(".mp4",'').gsub(".MOV",'').gsub(".MP4",'').gsub(".avi",'').gsub(".AVI",'')}.flv")
       File.open(flv, 'w')
-      system "ffmpeg -i #{ asset.path }  -ar 22050 -ab 192000 -s 480x360 -vcodec flv -r 25 -qscale 8 -f flv -y #{ flv }"
+      system "ffmpeg -i #{ asset.path }  -ar 22050 -ab 192000 -s 480x360 -vcodec flv -acodec libmp3lame -r 25 -qscale 8 -f flv -y #{ flv }"
       mp4 = File.join(File.dirname(asset.path), "#{self.asset_file_name.gsub(".mov",'').gsub(".flv",'').gsub(".MOV",'').gsub(".FLV",'').gsub(".avi",'').gsub(".AVI",'')}.mp4")
       File.open(mp4, 'w')
-      system "ffmpeg -i #{ asset.path } -ar 44100 -ab 128000 -s 640x480 -vcodec mpeg4 -aspect 16:9 -r 25 -qscale 8 -f mp4 -y #{ mp4 }"
+      system "ffmpeg -i #{ asset.path } -ar 44100 -ab 128000 -s 640x480 -vcodec mpeg4 -acodec libfaac -aspect 16:9 -r 25 -qscale 8 -f mp4 -y #{ mp4 }"
       grab_screenshot_from_video
     end
   end
@@ -94,10 +94,10 @@ class Article < ActiveRecord::Base
     logger.debug "Trying to grab a screenshot from #{asset.path}"
     flv = File.join(File.dirname(asset.path), "thumb135x100.jpg")
     File.open(flv, 'w')
-    system "ffmpeg -i #{ asset.path } -s 135x100 -vframes 10 -f image2 -an #{flv}"
+    system "ffmpeg -i #{ asset.path } -s 135x100 -vframes 1 -f image2 -an #{flv}"
     flv = File.join(File.dirname(asset.path), "thumb290x200.jpg")
     File.open(flv, 'w')
-    system "ffmpeg -i #{ asset.path } -s 290x200 -vframes 10 -f image2 -an #{flv}"
+    system "ffmpeg -i #{ asset.path } -s 290x200 -vframes 1 -f image2 -an #{flv}"
 
   end
 
